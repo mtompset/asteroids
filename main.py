2c3bc15fdba7338b0c37a8c3299377a418332fde
Mark Tompsett <mtompset@hotmail.com>
2025-08-02 16:19:21 -0400

    W,S,A,D working for player
    Starting attempt to use container groups.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from player import Player
+from circleshape import CircleShape
 from constants import *
 
 def main():
@@ -7,12 +9,24 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    game_clock = pygame.time.Clock()
+    dt = 0
+#    updateable = pygame.sprite.Group()
+#    drawable = pygame.sprite.Group()
+#    updateable.containers = (player)
+#    drawable.containers = (player)
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
