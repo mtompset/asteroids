1a27e394f8a1c4374885fb823fd2388b94f9048a
Mark Tompsett <mtompset@hotmail.com>
2025-08-02 17:14:36 -0400

    Base class circle shape file

---
+++ circleshape.py
@@ -0,0 +1,29 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen):
+        # sub-classes must override
+        pass
+
+    def update(self, dt):
+        # sub-classes must override
+        pass
+
+    def collision(self, other_circle):
+        pos1 = self.position
+        pos2 = other_circle.position
+        distance = pygame.math.Vector2.distance_to(pos1, pos2)
+        radial_length = self.radius + other_circle.radius
+        return distance <= radial_length
